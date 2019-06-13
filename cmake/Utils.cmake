bed5f38342db3b1488323107c15da188018397cd
hash_2000 <infinite_2000@mail.ru>
2019-06-13 22:29:37 +0500

    Подключение levelDb

---
+++ cmake/Utils.cmake
@@ -0,0 +1,9 @@
+################################################################################################
+# Clears variables from list
+# Usage:
+#   caffe_clear_vars(<variables_list>)
+macro(caffe_clear_vars)
+  foreach(_var ${ARGN})
+    unset(${_var})
+  endforeach()
+endmacro()
