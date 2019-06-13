1343f4924b0037be6a0d0591f90aa21934b13666
hash_2000 <infinite_2000@mail.ru>
2019-06-13 22:00:05 +0500

    настройка CMake

--- Server/src/Server.cpp
+++ Server/src/Server.cpp
@@ -1,2 +1,14 @@
-
+#include <stdafx.h>
 #include "Server.h"
+
+
+#include <Poco/SQL/PostgreSQL/Connector.h>
+
+
+
+int main()
+{
+    Poco::SQL::PostgreSQL::Connector::registerConnector();
+
+    return 0L;
+}
