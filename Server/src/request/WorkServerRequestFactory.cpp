d1f793898182e4c91b7776aa83634089cb020be7
hash_2000 <infinite_2000@mail.ru>
2019-06-20 21:33:45 +0500

    test page HomeController

--- Server/src/request/WorkServerRequestFactory.cpp
+++ Server/src/request/WorkServerRequestFactory.cpp
@@ -8,7 +8,8 @@
 Poco::Net::HTTPRequestHandler* WorkServerRequestFactory::createRequestHandler(
     const Poco::Net::HTTPServerRequest& request)
 {
-    if (request.getURI() == "/Home") {
+    if (request.getURI() == "/Home/Index" && 
+        request.getMethod() == Poco::Net::HTTPRequest::HTTP_GET) {
         return new HomeController;
     }
     return nullptr;
--- Server/src/request/controller/HomeController.cpp
+++ Server/src/request/controller/HomeController.cpp
@@ -1,4 +1,8 @@
 #include <stdafx.h>
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/StreamCopier.h>
+
 #include "HomeController.h"
 
 
@@ -6,5 +10,19 @@ void HomeController::handleRequest(
     Poco::Net::HTTPServerRequest& request, 
     Poco::Net::HTTPServerResponse& response)
 {
-    
+    if (request.getChunkedTransferEncoding())
+		response.setChunkedTransferEncoding(true);
+	else if (request.getContentLength() != Poco::Net::HTTPMessage::UNKNOWN_CONTENT_LENGTH)
+		response.setContentLength(request.getContentLength());
+	
+	response.setContentType(request.getContentType());
+	
+	//std::istream& istr = request.stream();
+	auto& ostr = response.send();
+    ostr << "<html>";
+    ostr << "<body>";
+    ostr << "<h3>тестовая строка</h3>";  
+    ostr << "</body>";
+    ostr << "</html>";
+    //Poco::StreamCopier::copyStream(istr, ostr);
 }
