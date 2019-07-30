1268334faa537f928632efa256c9808a83251e9d
hash2000 <infinite_2000@mail.ru>
2019-07-30 21:09:40 +0500

    Ext/vmx

---
+++ AppData/Web/www/Content/Scripts/Ext/vmx/app/SplitViewModel.js
@@ -0,0 +1,192 @@
+﻿/* global Ext */
+
+/**
+* This override encapsulates the ViewModel of a component.
+* This is achieved by giving unique names to the properties of the ViewModel.
+*/
+Ext.define('Ext.vmx.app.SplitViewModel', {
+    override: 'Ext.app.ViewModel',
+
+    config: {
+        /**
+        @cfg {String}
+        ViewModel name
+        */
+        name: undefined,
+
+        /**
+        @cfg {String}
+        @private
+        Identifier (name + sequential identifier)
+        */
+        uniqueName: undefined,
+
+        /**
+        @cfg {String}
+        @private
+        uniqueName + '-'
+        */
+        prefix: undefined
+    },
+
+    uniqueNameRe: /-id-\d+/,
+
+    privates: {
+        applyData: function (newData, data) {
+            newData = this.getPrefixedData(newData);
+            data = this.getPrefixedData(data);
+
+            return this.callParent([newData, data]);
+        },
+
+        applyLinks: function (links) {
+            links = this.getPrefixedData(links);
+            return this.callParent([links]);
+        },
+
+        applyFormulas: function (formulas) {
+            formulas = this.getPrefixedData(formulas);
+            return this.callParent([formulas]);
+        },
+
+        bindExpression: function (path, callback, scope, options) {
+            path = this.getPrefixedPath(path);
+            return this.callParent([path, callback, scope, options]);
+        }
+    },
+
+    linkTo: function (key, reference) {
+        key = this.getPrefixedPath(key);
+        return this.callParent([key, reference]);
+    },
+
+    get: function (path) {
+        path = this.getPrefixedPath(path);
+        return this.callParent([path]);
+    },
+
+    set: function (path, value) {
+        if (Ext.isString(path)) {
+            path = this.getPrefixedPath(path);
+        }
+        else if (Ext.isObject(path)) {
+            path = this.getPrefixedData(path);
+        }
+        this.callParent([path, value]);
+    },
+
+    /**
+    * The name is either a specified name 
+    * or a type of the ViewModel 
+    * or just 'viewmodel' (for anonymous ViewModels).
+    */
+    applyName: function (name) {
+        name = name || this.type || 'viewmodel';
+        return name;
+    },
+
+    /**
+    * Unique name is based on the Ext.id generator
+    */
+    applyUniqueName: function (uniqueName) {
+        uniqueName = uniqueName || Ext.id(null, this.getName() + '-id-');
+        return uniqueName;
+    },
+
+    /**
+    * Prefix is the unique name with the delimiter
+    */
+    applyPrefix: function (prefix) {
+        prefix = prefix || this.getUniqueName() + '-';
+        return prefix;
+    },
+
+    /**
+    Get the data object with the keys prefixed
+    */
+    getPrefixedData: function (data) {
+        var name, newName, value,
+            result;
+
+        if (!data) {
+            return null;
+        }
+
+        result = {};
+
+        for (name in data) {
+            value = data[name];
+            newName = this.getPrefixedPath(name);
+            result[newName] = value;
+        }
+
+        return result;
+    },
+
+    /**
+    Get the path with a correct prefix
+
+    Examples:
+
+        foo.bar -> myviewmodel-id-123-foo.bar
+        myviewmodel.foo.bar -> myviewmodel-id-123-foo.bar
+        myviewmodel-id-123-foo.bar -> myviewmodel-id-123-foo.bar (no change)
+
+    */
+    getPrefixedPath: function (path) {
+        var parts,
+            maybeHasViewModelName,
+            name,
+            vm;
+
+        // If there is a unique name in path
+        if (this.uniqueNameRe.test(path)) {
+            return path;
+        }
+        else {
+            maybeHasViewModelName = path.indexOf('.') > -1;
+
+            if (maybeHasViewModelName) {
+                // The descriptor may contain a name of a ViewModel: myviewmodel.foo.bar
+                parts = path.split('.');
+                name = parts[0];
+
+                vm = this.findViewModelByName(name);
+                if (vm) {
+                    // Binding to the specified ViewModel
+                    path = vm.getPrefix() + parts.slice(1).join('.');
+                }
+                else {
+                    // Binding to this ViewModel
+                    path = this.getPrefix() + path;
+                }
+            }
+            else {
+                // The descriptor doesn't contain the name of a ViewModel.
+                // Binding to this ViewModel
+                path = this.getPrefix() + path;
+            }
+        }
+
+        return path;
+    },
+
+    /**
+    Find a ViewModel by the name up the hierarchy
+    @param {String} name ViewModel's name
+    @param {Boolean} skipThis true to ignore this instance
+    */
+    findViewModelByName: function (name, skipThis) {
+        var result,
+            vm = skipThis ? this.getParent() : this;
+
+        while (vm) {
+            if (vm.getName() == name) {
+                return vm;
+            }
+            vm = vm.getParent();
+        }
+
+        return null;
+    }
+});
---
+++ AppData/Web/www/Content/Scripts/Ext/vmx/app/bind/Binding.js
@@ -0,0 +1,30 @@
+﻿/* global Ext */
+
+/**
+ * начиная с версии 6.2 из bind убрали обработку "!"
+ */
+Ext.define('Ext.vmx.app.bind.Binding', {
+    override: 'Ext.app.bind.Binding',
+
+    getValue: function () {
+        var me = this,
+            stub = me.stub,
+            ret = stub && stub.getValue();
+        if (me.not) {
+            ret = !ret;
+        }
+        return ret;
+    },
+
+    privates: {
+        getRawValue: function () {
+            var me = this,
+                stub = me.stub,
+                ret = stub && stub.getRawValue();
+            if (me.not) {
+                ret = !ret;
+            }
+            return ret;
+        }
+    }
+});
