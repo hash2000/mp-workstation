aa1b060dc1cd6cd24ba63521438f93d7ab0013f7
hash2000 <infinite_2000@mail.ru>
2019-08-22 20:45:25 +0500

    test ObjectAdd method

--- Server/src/request/controller/System/Objects/ObjectsAddController.cpp
+++ Server/src/request/controller/System/Objects/ObjectsAddController.cpp
@@ -24,11 +24,45 @@ Poco::JSON::Object::Ptr ObjectsAddController::HandleRequest(DatabaseManager* db,
     if (item.isEmpty())
         return json;
 
-    Poco::DynamicStruct model = *item.extract<Poco::JSON::Object::Ptr>();
+    json->set("total", 1);
+    json->set("success", true); 
 
-    // db->_SystemObjectsModel.Add(
-    //     model["parentId"].extract<std::size_t>());
-    
+    Poco::DynamicStruct model = *item.extract<Poco::JSON::Object::Ptr>();
+    SystemObject itemValue;
+
+    itemValue.ParentId = 0;
+    itemValue.Type = SystemObjectType::Directory;
+
+    auto parentIdvar = model["parentId"];
+    if (parentIdvar.isInteger()) {
+        itemValue.ParentId = parentIdvar.convert<std::size_t>();
+    }
+
+    auto typeIdvar = model["TypeId"];
+    if (typeIdvar.isInteger()) {
+        itemValue.Type = (SystemObjectType)typeIdvar.convert<int>();
+    }    
+
+    auto namevar = model["Name"];
+    if (namevar.isString()) {
+        itemValue.Name = namevar.convert<std::string>();
+    } 
+
+    auto result = db->_SystemObjectsModel.Add(
+        itemValue.ParentId,
+        itemValue.Type,
+        itemValue.Name
+    );
+
+    Poco::DynamicStruct ds;
+    ds["id"] = result->Id;
+    ds["parentId"] = result->ParentId;
+    ds["Name"] = result->Name;
+    ds["TypeId"] = result->Type;
+    ds["TypeName"] = result->TypeName;
+    ds["ModifiedDate"] = result->ModifiedDate;
+
+    json->set("items", ds);
 
     return json;
 }
